819ba4fb9267c33d8d213e6a006c95e05a697620
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-10-13 17:23:51 -0500

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,27 +1,29 @@
-console.log(firebase)
+console.log(firebase);
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
-const signInBtn = document.getElementById('login-btn');
-const GsignInBtn = document.getElementById('GLogin');
-const signOutBtn = document.getElementById('logout-btn');
+const signInBtn = document.getElementById("login-btn");
+const signOutBtn = document.getElementById("logout-btn");
 
 signOutBtn.style.display = "none";
 
 signInBtn.onclick = () => auth.signInWithPopup(provider);
-GsignInBtn.onclick = () => auth.signInWithPopup(provider);
+if (document.getElementById("GLogin")) {
+  const GsignInBtn = document.getElementById("GLogin");
+  GsignInBtn.onclick = () => auth.signInWithPopup(provider);
+}
 signOutBtn.onclick = () => auth.signOut();
 
-auth.onAuthStateChanged(user => {
-    if (user) {
-        // signed in
-        signInBtn.style.display = "none";
-        signOutBtn.style.display = "block";
-        alert("signed in");
-    } else {
-        // not signed in
-        signInBtn.style.display = "block";
-        signOutBtn.style.display = "none";
-        alert("signed out");
-    }
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // signed in
+    signInBtn.style.display = "none";
+    signOutBtn.style.display = "block";
+    console.log("signed in");
+  } else {
+    // not signed in
+    signInBtn.style.display = "block";
+    signOutBtn.style.display = "none";
+    console.log("signed out");
+  }
 });
