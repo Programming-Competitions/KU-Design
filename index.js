0bd68bf5b2a3e98578aeda10498c5e63047ea065
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-10-20 03:30:38 +0000

    up

--- index.js
+++ index.js
@@ -6,22 +6,47 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname });
 });
+app.get('/route', (req, res) => {
+  res.sendFile('routes.html', { root: __dirname });
+});
+
+this.current_user = NaN
 
 let listData = {
-  lists: {
-    sublist1: {
-      tasks: {
-        label: "description",
-        other_label: "other description",
+  users: {
+    vatsa: {
+      lists: {
+        sublist1: {
+          tasks: {
+            label: "description",
+            other_label: "other description",
+          },
+        },
+        sublist2: {
+          tasks: {
+            label: "description",
+            other_label: "other description",
+          },
+        },
       },
     },
-    sublist2: {
-      tasks: {
-        label: "description",
-        other_label: "other description",
+    raj: {
+      lists: {
+        sublist1: {
+          tasks: {
+            label: "description",
+            other_label: "other description",
+          },
+        },
+        sublist2: {
+          tasks: {
+            label: "description",
+            other_label: "other description",
+          },
+        },
       },
-    },
-  },
+    }
+  }
 };
 
 let authData = {
@@ -52,6 +77,18 @@ app.post('/register', async (req, res) => {
     password
   };
 
+  // add a notes route for them
+  listData.users[username] = {
+    lists: {
+      newList: {
+        tasks: {
+          label: "Welcome",
+          other_label: "Get work done!",
+        },
+      },
+    }
+  }
+
   res.status(200).send('User registered successfully');
 });
 
@@ -66,17 +103,44 @@ app.get('/login', async (req, res) => {
     return;
   }
 
-  // Check if the password is correct
-  if (JSON.stringify(authData.users[username].password) != password) {
+  // Check if the password is correct 
+  // dont use json stringify on this
+  if (authData.users[username].password != password) {
+    //console.log(authData.users[username].password === password);
+    //console.log(typeof authData.users[username].password === typeof password);
     res.status(403).send(`${typeof authData.users[username].password}:${typeof password} Incorrect password`);
     return;
   }
 
-  // Login the user
-  // TODO: Implement a more robust login mechanism, such as using JSON Web Tokens (JWTs)
-  req.session.user = username;
+  this.current_user = username
 
   res.status(200).send('User logged in successfully');
+});
+
+// Get all lists
+app.get('/todo', async (req, res) => {
+  if (this.current_user == NaN) {
+    res.status(403).send('Please Login to use this feature');
+  }
+
+  res.status(200).send(JSON.stringify(
+    listData.users[this.current_user]
+  ));
+});
+
+// get a specific list
+app.get('/list', async (req, res) => {
+  const lName = req.query.l;
+
+  if (this.current_user == NaN) {
+    res.status(403).send('Please Login to use this feature');
+  } if (listData.users[this.current_user] == undefined){
+    res.status(403).send('Please Login to use this feature');
+  }
+
+  res.status(200).send(JSON.stringify(
+    listData.users[this.current_user].lists[lName]
+  ));
 });
 
 // Start the server
